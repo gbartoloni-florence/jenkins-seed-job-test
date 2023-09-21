6bf1687f03242d48240710141657e13f3097c2ae
Giacomo Bartoloni FCG <63246312+gbartoloni-florence@users.noreply.github.com>
2023-09-21 15:04:15 +0200

    Fix

--- jobs/job1.groovy
+++ jobs/job1.groovy
@@ -1,6 +1,10 @@
 import groovy.io.FileType
 import java.util.logging.Logger
 
+// https://stackoverflow.com/questions/42146524/write-log-from-jenkinsfile
+Logger logger = Logger.getLogger('org.example.jobdsl')
+logger.info(message)
+
 
 def list = []
 
@@ -9,9 +13,6 @@ dir.eachFileRecurse (FileType.FILES) { file ->
   list << file
 }
 
-// https://stackoverflow.com/questions/42146524/write-log-from-jenkinsfile
-Logger logger = Logger.getLogger('org.example.jobdsl')
-logger.info('Hello from a Job DSL script!')
 
 list.each {
   logger.info(it.path)
@@ -22,11 +23,11 @@ list.each {
 
 jobarray = ['job1','job3']
 for(currentjob in jobarray)
-multibranchPipelineJob("$currentjob") { // normal variable syntax
-    branchSources {
-        git {
-            id = "^${currentjob}"       // accessing variable with escaping
-            remote('https://github.com/jenkinsci/configuration-as-code-plugin.git')
+    multibranchPipelineJob("$currentjob") { // normal variable syntax
+        branchSources {
+            git {
+                id = "^${currentjob}"       // accessing variable with escaping
+                remote('https://github.com/jenkinsci/configuration-as-code-plugin.git')
+            }
         }
     }
-}
