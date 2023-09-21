e650fd9a12a70f98218378092e1e9e8783f337c9
Giacomo Bartoloni FCG <63246312+gbartoloni-florence@users.noreply.github.com>
2023-09-21 12:48:54 +0200

    Fix

---
+++ jobs/job1.groovy
@@ -0,0 +1,10 @@
+jobarray = ['job1','job2']
+for(currentjob in jobarray)
+multibranchPipelineJob("$currentjob") { // normal variable syntax
+    branchSources {
+        git {
+            id = "^${currentjob}"       // accessing variable with escaping
+            remote('https://github.com/jenkinsci/configuration-as-code-plugin.git')
+        }
+    }
+}
