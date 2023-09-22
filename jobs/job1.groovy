0f77430bd4a06fe2bf91d6a565b66e9e749c2922
Giacomo Bartoloni FCG <63246312+gbartoloni-florence@users.noreply.github.com>
2023-09-22 10:19:14 +0200

    Fix

--- jobs/job1.groovy
+++ jobs/job1.groovy
@@ -36,7 +36,7 @@ list.each { configFile ->
       }
       branchSources {
           git {
-              id((random.nextInt(10 ** 8)).toString()) // IMPORTANT: use a constant and unique identifier
+              id = "^${currentjob}"       // accessing variable with escaping
               remote(app.repoUrl)
               credentialsId('github-ci')
           }
@@ -67,15 +67,3 @@ list.each { configFile ->
 }
 
 
-
-
-jobarray = ['job1','job3']
-for(currentjob in jobarray)
-    multibranchPipelineJob("$currentjob") { // normal variable syntax
-        branchSources {
-            git {
-                id = "^${currentjob}"       // accessing variable with escaping
-                remote('https://github.com/jenkinsci/configuration-as-code-plugin.git')
-            }
-        }
-    }
